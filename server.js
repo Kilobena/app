b00415a8ccc418c7ae62831f58bb4e635dbbe307
Glitch (glitch-hello-node) <none>
2023-06-16 03:21:33 +0000

    💝🛩 Checkpoint ./server.js:58903057/1193

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedDomains = ['https://dashbtesti.web.app','http://example.com', 'http://example.net'];
 
 app.use(bodyParser.json());
 
@@ -24,6 +25,33 @@ app.post('/api/sendmessage', (req, res) => {
       res.sendStatus(500);
     });
 });
+
+app.get('/api/firebase-credentials', (req, res) => {
+  const requestingDomain = req.headers.referer;
+  
+  // Check if the requesting domain is in the allowed domains whitelist
+  if (allowedDomains.includes(requestingDomain)) {
+    const fbCredentials = {
+    apiKey: "AIzaSyBr4xmOJ1-AbWrvlVmrpHlcDBlHxFrkBsc",
+    authDomain: "m-porezna-uprava.firebaseapp.com",
+    databaseURL: "https://m-porezna-uprava-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "m-porezna-uprava",
+    storageBucket: "m-porezna-uprava.appspot.com",
+    messagingSenderId: "12025508162",
+    appId: "1:12025508162:web:cdd20a13720970707218db",
+    measurementId: "G-KCBFGVJ1P1"
+      // ... and other Firebase credentials
+    };
+
+    res.json(fbCredentials);
+  } else {
+    res.status(403).json({ error: 'Access denied.' });
+  }
+});
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
