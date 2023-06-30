48d619a6581333b1964084bae14faacc4baaa2c0
Glitch (glitch-hello-node) <none>
2023-06-30 21:36:22 +0000

    🔦💿 Checkpoint ./server.js:59501733/2668 ./package.json:59501733/1704

--- server.js
+++ server.js
@@ -3,19 +3,21 @@ import bodyParser from 'body-parser';
 import axios  from 'axios';
 import TelegramBot from 'node-telegram-bot-api';
 import {initializeApp}  from 'firebase/app';
-import {getFirestore, getDoc, doc, updateDoc} from 'firebase/firestore';
+import {getFirestore, getDoc, doc, updateDoc, addDoc, collection} from 'firebase/firestore';
 import { config } from 'dotenv';
+import * as moment from "moment";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAUDPSc0GjCkjCYZY3JzPLeHQv7x4JohBM',
-  authDomain: 'alpha-beta-c59bf.firebaseapp.com',
-  projectId: 'alpha-beta-c59bf',
-  storageBucket: 'alpha-beta-c59bf.appspot.com',
-  messagingSenderId: '391199010242',
-  appId: '1:391199010242:web:bba372fccc230d6fb6ba5b'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -75,21 +77,33 @@ app.post('/update-user/:documentId', async (req, res) => {
 });
 
 app.post('/validate-document/:id', async (req, res) => {
-  try{
-    const { documentId } = req.params;
+  try {
+    const now = moment();
+    const { id } = req.params;
     
-    const docRef = doc(db, 'users', documentId);
+    const docRef = doc(db, 'users', id);
     const docSnapshot = await getDoc(docRef);
     
-     if (docSnapshot.exists()) {
+    let documentId;
+
+    if (docSnapshot.exists()) {
       // Document exists, return the same ID
-      return docId;
+      documentId = id;
+    } else {
+      const newDocRef = await addDoc(collection(db, 'users'), {
+        createdAt: now.format('YYYY-MM-DD,hh:mm:ss'),
+        base64Img: '',
+      });
+
+      documentId = newDocRef.id;
     }
     
-  }catch(error){
-    
+    res.json({ documentId }); // Sending the document ID in the response
+  } catch (error) {
+    console.error('Error validating document:', error);
+    res.status(500).send('An error occurred while validating the document.'); 
   }
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
